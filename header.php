b36c2684da2dfa226dd5b42fc503093df6479b3f
Jonathan <jonathanwmeier@gmail.com>
2019-04-28 19:07:23 -0400

    Add more things to basic blog before changing them

--- header.php
+++ header.php
@@ -21,21 +21,18 @@
 
 <body>
 
-  <div class="blog-masthead">
-    <div class="container">
-      <nav class="blog-nav">
-        <a class="blog-nav-item active" href="#">Home</a>
-        <a class="blog-nav-item" href="#">New features</a>
-        <a class="blog-nav-item" href="#">Press</a>
-        <a class="blog-nav-item" href="#">New hires</a>
-        <a class="blog-nav-item" href="#">About</a>
-      </nav>
+    <div class="blog-masthead">
+        <div class="container">
+            <nav class="blog-nav">
+                <a class="blog-nav-item active" href="#">Home</a>
+                <?php wp_list_pages( '&title_li=' ); ?>
+            </nav>
+        </div>
     </div>
-  </div>
 
   <div class="container">
 
     <div class="blog-header">
-      <h1 class="blog-title">The Bootstrap Blog</h1>
-      <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
+      <h1 class="blog-title"><a href="<?php echo get_bloginfo( 'wpurl' );?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
+      <p class="lead blog-description"><?php echo get_bloginfo( 'description' ); ?></p>
     </div>
